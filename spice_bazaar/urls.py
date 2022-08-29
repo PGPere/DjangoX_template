98489a24e6b894f4042ca530b0d471cc60a900ae
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-08-29 17:47:55 -0400

    CRUD is operational

--- spice_bazaar/urls.py
+++ spice_bazaar/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 
 from .views import HomePageView
+from .views import SpiceDetail, SpiceList
+from .views import SpiceCreateView, SpiceDeleteView, SpiceUpdateView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="spice_bazaar_home"), ]
+    path("", HomePageView.as_view(), name="spice_bazaar_home"),
+    path(' ', SpiceList.as_view(), name='spice_list'),
+    path('<int:pk>', SpiceDetail.as_view(), name='spice_detail'),
+    path('create/', SpiceCreateView.as_view(), name='spice-add'),
+    path('<int:pk>/update/',
+         SpiceUpdateView.as_view(), name='spice-update'),
+    path('<int:pk>/delete/', SpiceDeleteView.as_view(), name='spice-delete'),
+]
--- spice_bazaar/views.py
+++ spice_bazaar/views.py
@@ -1,5 +1,37 @@
 from django.views.generic import TemplateView
+from spice_bazaar.models import Spice_bazaar
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 class HomePageView(TemplateView):
     template_name = "pages/spice_bazaar_home.html"
+
+
+class SpiceList(ListView):
+    template_name = "spice_list.html"
+    model = Spice_bazaar
+    context_object_name = 'sp_list'
+
+
+class SpiceDetail(DetailView):
+    template_name = "spice_detail.html"
+    model = Spice_bazaar
+
+
+class SpiceCreateView(CreateView):
+    template_name = 'spice-add.html'
+    model = Spice_bazaar
+    fields = ['name', 'description', 'purchaser']
+
+
+class SpiceUpdateView(UpdateView):
+    template_name = 'spice-update.html'
+    model = Spice_bazaar
+    fields = ['name', 'description', 'purchaser']
+
+
+class SpiceDeleteView(DeleteView):
+    template_name = 'spice-delete.html'
+    model = Spice_bazaar
+    success_url = reverse_lazy('spice_list')
